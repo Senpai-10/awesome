9ad2464d85913421665ad829b90af64e86022bac
abdalrzag eisa <bmjfdrh@gmail.com>
2022-10-24 02:52:57 +0300

    fix run

--- autorun.sh
+++ autorun.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-function run {
-    (ps -AL | grep -i cmus-rpc) || ${@:2} &
-    #if ! pgrep $1 ; then
-    #    ${@:2}&
-    #fi
+run() {
+    if ! pgrep "$1" > /dev/null ;
+    then
+        "${@:2}"&
+    fi
 }
 
-run picom       picom
-run nitrogen    nitrogen --restore
-run imwheel     imwheel -b 45
-run flameshot   flameshot
-run gromit-mpx  gromit-mpx
-run Discord     discord --start-minimized
-run qpwgraph    qpwgraph ~/patchbay.qpwgraph -am
-
-kill $(ps -AL | grep -i discover-overla | awk '{print $2}') ; discover-overlay &
+run picom           picom
+run nitrogen        nitrogen --restore
+run imwheel         imwheel -b 45
+run flameshot       flameshot
+run gromit-mpx      gromit-mpx
+run Discord         discord --start-minimized
+run qpwgraph        qpwgraph ~/patchbay.qpwgraph -am
+run discover-overla discover-overlay
