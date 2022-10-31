8b52f0675cb7f7809e202d8290eb61779f44fd51
abdalrzag eisa <bmjfdrh@gmail.com>
2022-10-31 08:05:04 +0300

    remove backgound image

--- themes/main/theme.lua
+++ themes/main/theme.lua
@@ -124,8 +124,6 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."main/titlebar/ma
 theme.titlebar_maximized_button_normal_active = themes_path.."main/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."main/titlebar/maximized_focus_active.png"
 
--- theme.wallpaper = themes_path.."main/background.png"
-
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."main/layouts/fairhw.png"
 theme.layout_fairv = themes_path.."main/layouts/fairvw.png"
