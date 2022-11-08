30ba7f744a0c334b94a72b7d7d55bb61e46fcfe1
abdalrzag eisa <bmjfdrh@gmail.com>
2022-11-08 12:55:10 +0300

    set client border width to 0

--- themes/main/theme.lua
+++ themes/main/theme.lua
@@ -26,7 +26,7 @@ theme.fg_urgent = "#ffffff"
 theme.fg_minimize = "#ffffff"
 
 theme.useless_gap = dpi(5)
-theme.border_width = dpi(1)
+theme.border_width = dpi(0)
 theme.border_normal = "#535d6c"
 theme.border_focus = "#e8333f"
 theme.border_marked = "#FFFFFF"
