be12d247cba59ad70fa561505754aca8e08e4dc9
abdalrzag eisa <bmjfdrh@gmail.com>
2022-11-09 16:17:48 +0300

    only show uptime when on hover

--- lua/rc/widgets/uptime.lua
+++ lua/rc/widgets/uptime.lua
@@ -1,15 +1,16 @@
 local awful = require("awful")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
-local get_script = require("lua.helpers.get_script").get_script
 local gears = require("gears")
 
 local M = {}
 
+local watch_widget = wibox.widget.textbox("神  uptime")
+
 M.widget = {
 	{
 		{
-			widget = awful.widget.watch("bash -c " .. get_script("uptime.sh"), 15),
+			widget = watch_widget,
 		},
 		left = 10,
 		right = 10,
@@ -18,9 +19,22 @@ M.widget = {
 		widget = wibox.container.margin,
 	},
 	fg = beautiful.widget_fg,
-    bg = beautiful.widget_bg,
-    shape = gears.shape.rounded_rect,
+	bg = beautiful.widget_bg,
+	shape = gears.shape.rounded_rect,
 	widget = wibox.container.background,
 }
+
+local uptime_tooltip = awful.tooltip({})
+
+uptime_tooltip:add_to_object(watch_widget)
+
+watch_widget:connect_signal("mouse::enter", function()
+	local handle = assert(io.popen("uptime -p"))
+	local result = handle:read("*a")
+
+	uptime_tooltip.text = result
+
+	handle:close()
+end)
 
 return M
