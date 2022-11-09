7f15477705e3229d0408889175733b337a57b674
abdalrzag eisa <bmjfdrh@gmail.com>
2022-11-09 16:18:28 +0300

    remove uptime script

--- scripts/uptime.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# uptime=$(cat /proc/uptime | perl -ne '/(\d*)/ ; printf "%02d:%02d:%02d:%02d\n",int($1/86400),int(($1%86400)/3600),int(($1%3600)/60),$1%60')
-uptime=$(uptime -p)
-
-echo "神  $uptime"
