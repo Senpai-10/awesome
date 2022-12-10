664644d8083fd1cca4c9a171eceb58f2f2d29f53
abdalrzag eisa <bmjfdrh@gmail.com>
2022-12-10 23:33:01 +0300

    change 'imwheel', 'cmus-rpc'

--- autorun.sh
+++ autorun.sh
@@ -19,11 +19,11 @@ run_or_restart() {
 
 run picom           picom
 run nitrogen        nitrogen --restore
-run imwheel         imwheel -b 45
+imwheel -kill -b 45
 run flameshot       flameshot
 run gromit-mpx      gromit-mpx
 run Discord         discord --start-minimized
 run qpwgraph        qpwgraph ~/patchbay.qpwgraph -am
 run nm-applet       nm-applet --indicator
 run_or_restart discover-overla discover-overlay
-run_or_restart cmus-rpc cmus-rpc
+cmus-rpc -k
