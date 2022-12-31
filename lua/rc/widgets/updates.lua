23e7a31b9812de8654ebb0786181c20a1fbacaa4
abdalrzag eisa <bmjfdrh@gmail.com>
2023-01-01 02:41:53 +0300

    left click to update

--- lua/rc/widgets/updates.lua
+++ lua/rc/widgets/updates.lua
@@ -4,10 +4,15 @@ local beautiful = require("beautiful")
 local get_script = require("lua.helpers.get_script").get_script
 local gears = require("gears")
 local naughty = require("naughty")
+local default = require("lua.rc.default")
 
 local watch_widget = awful.widget.watch("bash -c " .. get_script("updates.sh"), 1800)
 
 watch_widget:connect_signal("button::press", function(_, _, _, button)
+	if button == 1 then
+		awful.spawn(default.terminal .. " -e " .. "sudo pacman -Syu")
+	end
+
 	-- right mouse button
 	if button == 3 then
 		local result = io.popen("checkupdates"):read("*a")
