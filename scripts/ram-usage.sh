dbdb429d304d971f2e99a91a4d2f796e84ceac8a
abdalrzag eisa <bmjfdrh@gmail.com>
2023-01-01 06:50:30 +0300

    add ram_used_prec

--- scripts/ram-usage.sh
+++ scripts/ram-usage.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# ram_prec=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }' | perl -nl -MPOSIX -e 'print floor($_)')
+ram_used_prec=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }' | perl -nl -MPOSIX -e 'print floor($_)')
 ram_total="$(free -h | gawk  '/Mem:/{print $2}')"
 ram_used="$(free -h | gawk  '/Mem:/{print $3}')"
 ram_available="$(free -h | gawk  '/Mem:/{print $7}')"
 
-echo " $ram_used / $ram_total | $ram_available"
+echo " $ram_used / $ram_total ($ram_used_prec%)"
