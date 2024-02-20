6dae65206db08643844e11459f9695de3c7a8370
abdalrzag eisa <bmjfdrh@gmail.com>
2024-02-21 01:08:36 +0300

    add colors to output

--- lua/rc/widgets/cpu-temp.lua
+++ lua/rc/widgets/cpu-temp.lua
@@ -6,21 +6,66 @@ local gears = require("gears")
 
 local M = {}
 
-M.widget = {
+local cpu_temp_clr = wibox.widget.background()
+
+local label = wibox.widget({
+	{
+		widget = wibox.widget.textbox,
+		markup = "<b>CPU temp:</b>",
+	},
+	left = 10,
+	right = 10,
+	top = 3,
+	bottom = 3,
+	widget = wibox.container.margin,
+})
+
+local cpu_temp = wibox.widget({
 	{
 		{
-			widget = awful.widget.watch("bash -c " .. get_script("cpu-temp.sh"), 3),
+			{
+				id = "cpu_temp_label",
+				text = " ",
+				widget = wibox.widget.textbox,
+			},
+			fg = beautiful.widget_fg,
+			widget = wibox.container.background,
 		},
-		left = 10,
-		right = 10,
-		top = 3,
-		bottom = 3,
-		widget = wibox.container.margin,
+		{
+			{
+				widget = awful.widget.watch(
+					"bash -c 'cat /sys/class/thermal/thermal_zone0/temp'",
+					3,
+					function(widget, stdout)
+						local temp = tonumber(stdout) / 1000
+						widget:set_text(temp)
+
+						if temp > 80 then
+							cpu_temp_clr:set_fg("#FE0000")
+						elseif temp > 60 then
+							cpu_temp_clr:set_fg("#FD8C00")
+						else
+							cpu_temp_clr:set_fg("#32CD31")
+						end
+					end
+				),
+			},
+			widget = wibox.container.background,
+		},
+		layout = wibox.layout.align.horizontal,
 	},
-	fg = beautiful.widget_fg,
-	-- bg = beautiful.widget_bg,
-	shape = gears.shape.rectangle,
-	widget = wibox.container.background,
-}
+	left = 10,
+	right = 10,
+	top = 3,
+	bottom = 3,
+	widget = wibox.container.margin,
+})
+
+cpu_temp_clr:set_widget(cpu_temp)
+-- cpu_temp_clr:set_fg(beautiful.widget_fg)
+-- cpu_temp_clr:set_shape(gears.shape.rectangle)
+-- cpu_temp_clr:set_bg(beautiful.widget_bg)
+
+M.widget = cpu_temp_clr
 
 return M
