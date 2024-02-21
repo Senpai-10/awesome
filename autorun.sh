ce77da0cdb17a5d14e8fcb8c39c47f51de269e8d
abdalrzag eisa <bmjfdrh@gmail.com>
2024-02-21 23:44:59 +0300

    Fix autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 run() {
-    if ! pgrep "$1" > /dev/null ;
+    if ! pgrep -f "$1" > /dev/null ;
     then
         "${@:2}"&
     fi
 }
 
 run_or_restart() {
-    if pgrep "$1" > /dev/null
+    if pgrep -f "$1" > /dev/null
     then
-        killall "$1"
+        pkill -f "$1"
         "${@:2}"&
     else
         "${@:2}"&
@@ -26,5 +26,5 @@ run Discord         discord --start-minimized
 run qpwgraph        qpwgraph ~/patchbay.qpwgraph -am
 run nm-applet       nm-applet --indicator
 run steam steam -silent
-run_or_restart discover-overla discover-overlay
+run_or_restart discover-overlay discover-overlay
 cmus-rpc -k
