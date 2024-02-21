80d64c1c9f7018d2e4317cd41f39d0e727052d9b
abdalrzag eisa <bmjfdrh@gmail.com>
2024-02-21 20:41:10 +0300

    add colors to cpu usage widget

--- lua/rc/widgets/cpu-temp.lua
+++ lua/rc/widgets/cpu-temp.lua
@@ -7,37 +7,36 @@ local markup_clr = require("lua.helpers.markup_clr")
 
 local M = {}
 
-local cpu_temp_clr = wibox.widget.background()
-
-local cpu_temp = wibox.widget({
+M.widget = {
 	{
-		widget = awful.widget.watch("bash -c 'cat /sys/class/thermal/thermal_zone0/temp'", 3, function(widget, stdout)
-			local temp = tonumber(stdout) / 1000
-			local color = ""
 
-			if temp > 80 then
-				color = beautiful.palette.bright_red
-			elseif temp > 60 then
-				color = beautiful.palette.bright_orange
-			else
-				color = beautiful.palette.bright_green
-			end
+		{
+			widget = awful.widget.watch(
+				"bash -c 'cat /sys/class/thermal/thermal_zone0/temp'",
+				3,
+				function(widget, stdout)
+					local temp = tonumber(stdout) / 1000
+					local color = ""
 
-			widget:set_markup(" " .. markup_clr(temp, color) .. "°C")
-		end),
+					if temp > 80 then
+						color = beautiful.palette.bright_red
+					elseif temp > 60 then
+						color = beautiful.palette.bright_orange
+					else
+						color = beautiful.palette.bright_green
+					end
+
+					widget:set_markup(" " .. markup_clr(temp, color) .. "°C")
+				end
+			),
+		},
+		left = 10,
+		right = 10,
+		top = 3,
+		bottom = 3,
+		widget = wibox.container.margin,
 	},
-	left = 10,
-	right = 10,
-	top = 3,
-	bottom = 3,
-	widget = wibox.container.margin,
-})
-
-cpu_temp_clr:set_widget(cpu_temp)
--- cpu_temp_clr:set_fg(beautiful.widget_fg)
--- cpu_temp_clr:set_shape(gears.shape.rectangle)
--- cpu_temp_clr:set_bg(beautiful.widget_bg)
-
-M.widget = cpu_temp_clr
+	widget = wibox.widget.background(),
+}
 
 return M
--- lua/rc/widgets/cpu-usage.lua
+++ lua/rc/widgets/cpu-usage.lua
@@ -3,13 +3,27 @@ local wibox = require("wibox")
 local beautiful = require("beautiful")
 local gears = require("gears")
 local get_script = require("lua.helpers.get_script").get_script
+local markup_clr = require("lua.helpers.markup_clr")
 
 local M = {}
 
 M.widget = {
 	{
 		{
-			widget = awful.widget.watch("bash -c " .. get_script("cpu-usage.sh"), 3),
+			widget = awful.widget.watch("bash -c " .. get_script("cpu-usage.sh"), 3, function(widget, stdout)
+				local cpu_usage = tonumber(stdout)
+				local color = ""
+
+				if cpu_usage > 90 then
+					color = beautiful.palette.bright_red
+				elseif cpu_usage > 60 then
+					color = beautiful.palette.bright_orange
+				else
+					color = beautiful.palette.bright_green
+				end
+
+				widget:set_markup("  " .. markup_clr(cpu_usage .. "%", color))
+			end),
 		},
 		left = 10,
 		right = 10,
