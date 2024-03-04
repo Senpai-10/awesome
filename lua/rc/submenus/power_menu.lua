60e75fb15fe127a93cfc8cf7a65a9a33d1ec9600
abdalrzag eisa <bmjfdrh@gmail.com>
2024-03-04 22:06:07 +0300

    use systemctl with poweroff and reboot

--- lua/rc/submenus/power_menu.lua
+++ lua/rc/submenus/power_menu.lua
@@ -4,20 +4,37 @@ return {
 	{
 		"Poweroff",
 		function()
-			awful.spawn.with_shell("poweroff")
+			awful.spawn.with_shell("systemctl poweroff")
 		end,
 	},
 	{
 		"Reboot",
 		function()
-			awful.spawn.with_shell("reboot")
+			awful.spawn.with_shell("systemctl reboot")
 		end,
 	},
 	{
-		"Suspend",
+		-- Suspend saves its state to the RAM
+		"Suspend (RAM)",
 		function()
 			awful.spawn.with_shell("systemctl suspend")
 		end,
+	},
+	{
+		-- Hibernate, or Suspend to Disk
+		"Hibernate (DISK)",
+		function()
+			awful.spawn.with_shell("systemctl hibernate")
+		end,
+	},
+	{
+		-- hybrid-sleep Hibernate and suspend the system
+		-- 	so the power on is faster than powering on a computer 
+		-- 	and restoring the state manually.
+		"Hybrid sleep",
+		function()
+			awful.spawn.with_shell("systemctl hybrid-sleep")
+		end
 	},
 	{
 		"Quit",
