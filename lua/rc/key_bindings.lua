e83f5d17d6c52cb3127e21c31303a5f4ae343df6
abdalrzag eisa <bmjfdrh@gmail.com>
2024-03-23 02:46:00 +0300

    Add new keybindings

--- lua/rc/key_bindings.lua
+++ lua/rc/key_bindings.lua
@@ -147,6 +147,12 @@ local globalkeys = gears.table.join(
 
 	awful.key({ modkey, "Control" }, "r", awesome.restart, { description = "reload awesome", group = "awesome" }),
 	awful.key({ modkey, "Shift" }, "q", awesome.quit, { description = "quit awesome", group = "awesome" }),
+	awful.key({ modkey }, "q", function()
+		awful.spawn("quick-edit-list")
+	end, { description = "quit awesome", group = "quick list" }),
+	awful.key({ modkey, altkey }, "q", function()
+		awful.spawn("tmux-sessions-list")
+	end, { description = "quit awesome", group = "quick list" }),
 
 	awful.key({ modkey }, "l", function()
 		awful.tag.incmwfact(0.05)
