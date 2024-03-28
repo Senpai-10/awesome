ea84627ba6c5539539fb8d72d0d4cb239ee8ff49
abdalrzag eisa <bmjfdrh@gmail.com>
2024-03-28 06:18:56 +0300

    autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -21,11 +21,11 @@ run_or_restart() {
 run nitrogen        nitrogen --restore
 imwheel -kill -b 45
 run flameshot       flameshot
-# run gromit-mpx      gromit-mpx
-# run Discord         discord --start-minimized
+run gromit-mpx      gromit-mpx
+run Discord         discord --start-minimized
 run qpwgraph        qpwgraph ~/patchbay.qpwgraph -am
 run nm-applet       nm-applet --indicator
-# run thunderbird     thunderbird
-# run steam steam -silent
-# run_or_restart discover-overlay discover-overlay
-# cmus-rpc -k
+run thunderbird     thunderbird
+run steam steam -silent
+run_or_restart discover-overlay discover-overlay
+cmus-rpc -k
