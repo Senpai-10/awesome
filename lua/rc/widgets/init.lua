8ea9933454348a97af06438c04b92466cf200ad9
Abdalrzag Eisa <bmjfdrh@gmail.com>
2024-09-20 10:18:33 +0300

    remove weather widget

--- lua/rc/widgets/init.lua
+++ lua/rc/widgets/init.lua
@@ -3,7 +3,6 @@ local M = {}
 M.cmus = require("lua.rc.widgets.cmus").widget
 M.network = require("lua.rc.widgets.network").widget
 M.keyboardlayout = require("lua.rc.widgets.keyboardlayout").widget
-M.weather = require("lua.rc.widgets.weather").widget
 M.uptime = require("lua.rc.widgets.uptime").widget
 M.cpu_usage = require("lua.rc.widgets.cpu-usage").widget
 M.cpu_temp = require("lua.rc.widgets.cpu-temp").widget
